be24f88f8a5215e6868007ed7ec435973e446f66
robinleruth <robin.leruth@gmail.com>
2020-08-16 03:06:40 -0400

    r

--- app/interface/web/controllers/dispatch_service_controller.py
+++ app/interface/web/controllers/dispatch_service_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import status
 from typing import List
 
 from app.domain.services.dispatch_service import DispatchService
@@ -10,14 +11,16 @@ from app.domain.model.client import Client
 router = APIRouter()
 
 
-@router.post('/byClient', status_code=201, response_model=Output)
+@router.post('/byClient', status_code=status.HTTP_201_CREATED,
+             response_model=Output)
 async def process_clients(client: Client,
                           service: DispatchService = Depends(DispatchService)):
     res = service.process_client(client)
     return res
 
 
-@router.post('/byClients', status_code=201, response_model=List[Output])
+@router.post('/byClients', status_code=status.HTTP_201_CREATED,
+             response_model=List[Output])
 async def process_clients(clients: List[Client],
                           service: DispatchService = Depends(DispatchService)):
     res = service.process_clients(clients)
