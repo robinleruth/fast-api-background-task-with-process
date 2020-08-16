8198a9fea91efeac4133deddf6fe379fb1d4ba80
robinleruth <robin.leruth@gmail.com>
2020-08-16 16:58:51 -0400

    ping redis to know if available and which cache use

--- app/domain/services/cache_service/bean.py
+++ app/domain/services/cache_service/bean.py
@@ -1,10 +1,21 @@
+import redis
+
 from functools import lru_cache
 
 from app.domain.services.cache_service.cache_service import CacheService
+from app.domain.services.cache_service.redis_cache_service import RedisCacheService
 from app.infrastructure.db_connector.mock_db_connector import MockDBConnector
+from app.infrastructure.log import logger
 
 
 @lru_cache
 def get_cache_service() -> CacheService:
     connector = MockDBConnector()
-    return CacheService(connector)
+    r = redis.Redis()
+    try:
+        r.ping()
+        logger.info('Redis connection available')
+        service = RedisCacheService(connector)
+    except:
+        service = CacheService(connector)
+    return service
