8db3558207353dc494dd978f76ec31f6fa93ba1a
robinleruth <robin.leruth@gmail.com>
2020-08-16 17:20:25 -0400

    indent

--- app/infrastructure/db_connector/mock_db_connector.py
+++ app/infrastructure/db_connector/mock_db_connector.py
@@ -10,4 +10,5 @@ class MockDBConnector(DbConnector):
     lst = ['a', 'b', 'c' ,'d']
 
     def get_by_date(self, date: str) -> List[SomeModel]:
-        return [SomeModel(a=random.randint(1, 5), b=random.choice(lst)) for _ in range(random.randint(1, 5))]
+        return [SomeModel(a=random.randint(1, 5), b=random.choice(self.lst))
+                for _ in range(random.randint(2, 5))]
