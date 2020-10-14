75caf7d4a559153a2b5d33c0d81404bf94ed4592
robinleruth <robin.leruth@gmail.com>
2020-10-14 16:52:47 +0000

    docker compose

--- setup-host.sh
+++ setup-host.sh
@@ -12,4 +12,7 @@ pip3 install --upgrade pip wheel
 curl -fsSL https://get.docker.com/ | sh
 usermod -aG docker vagrant
 
-apt-get install docker-compose
+apt-get remove docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
